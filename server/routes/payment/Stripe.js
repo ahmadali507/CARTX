e73de99ef3f7a83ff6f377e4a26e1c1e2dfebca2
ahmadali507 <ashahid.bese23seecs@seecs.edu.pk>
2024-07-28 19:56:00 +0500

    add

--- server/routes/payment/Stripe.js
+++ server/routes/payment/Stripe.js
@@ -94,8 +94,8 @@ PaymentRouter.post('/create-checkout-session', async (req, res) => {
         }
       },
       mode: 'payment',
-      success_url: 'https://Cartx-mern507/success', // Your success URL
-      cancel_url: 'https://Cartx-mern507/cancel', // Your cancel URL
+      success_url: 'https://Cartx-mern507.netlify.app/success', // Your success URL
+      cancel_url: 'https://Cartx-mern507.netlify.app/cancel', // Your cancel URL
     });
 
     res.json({ id: session.id });
